11a6d7fdf6393d6d8f684041c40de61dd276fd28
krup13 <khairanafisa4@gmail.com>
2024-05-08 03:51:23 +0800

    debug dependencies in pom.xml. any problems regarding that file can ask. function to access google apis added in mailconfig class

---
+++ src/main/java/com/TweetForge/TweetForge/backend/config/MailConfiguration.java
@@ -0,0 +1,56 @@
+package com.TweetForge.TweetForge.backend.config;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.GmailScopes;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class MailConfiguration {
+
+    //refer google's official github for setup
+    private static final String ApplicationName = "TweetForge";
+
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
+
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    //retrieving credentials for accessing Google APIs
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+
+        InputStream in = MailConfiguration.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Credentials file not found");
+        }
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                        .setAccessType("offline")
+                            .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+
+}
